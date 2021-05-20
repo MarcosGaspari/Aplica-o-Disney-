6d13f4fe48fa051e854d91e12810a8355133261e
marcos gaspari <marcosgaspari098@hotmail.com>
2021-05-20 00:03:38 -0300

    Mongodb login e cadastro

--- Projetin/index.js
+++ Projetin/index.js
@@ -2,7 +2,8 @@ let http = require('http'),
     path = require('path'),
     express = require('express'),
     app = express(),
-    Posts = require('./model/posts');
+    Posts = require('./model/posts'),
+    Cadastro = require('./model/posts');
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'view'));
@@ -13,7 +14,14 @@ app.get('/Disney', (req, res) =>{
     res.render('Index');
 });
 app.get('/posts',async(req, res) =>{
-    const posts = await Posts.find();
+    const   busca = req.query.busca,
+            senha = req.query.senha,
+            posts = await Posts.find(busca, senha);
     res.render('posts', {posts: posts});
-})
+});
+app.post('/cadastro',async(req, res) =>{
+    const adicionar = req.body.adicionar;
+    const password = req.body.password;
+    Cadastro.insert(adicionar,password);
+});
 app.listen(3000);
--- Projetin/model/Posts.js
+++ Projetin/model/Posts.js
@@ -1,9 +1,18 @@
 const MongoClient = require('mongodb').MongoClient;
 
 module.exports = class Posts{
-    static async find(){
-        const conn = await MongoClient.connect('mongodb://localhost:27017/exemplo01');
+    static async find(busca, senha){
+        const conn = await MongoClient.connect('mongodb://localhost:27017/Projeto');
         const db = conn.db();
-        return await db.collection('posts').find().toArray();
+        if (busca && senha){
+            return await db.collection('usuarioSenha').find({Usuario:(busca), senha: (senha) }).toArray();
+        }
     }
-}
+}   
+//module.exports = class Cadastro{
+//    static async insert(adicionar,password){
+  //      conn = await MongoClient.connect('mongodb://localhost:27017/Projeto');
+    //        db.conn.db();
+      //  db.collection('usuarioSenha').insertOne({Usuario: adicionar , senha: password});
+    //}
+//}
